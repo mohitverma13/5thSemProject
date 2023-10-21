b78c5c50aec35bf03585b724ad6aa02464959b8e
Mohit Kumar <mohitverma8822@gmail.com>
2023-10-21 23:28:43 +0530

    working perfect till login sign up and reset password

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import OpenRoute from "./components/core/Auth/OpenRoute"
 
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
+import ForgotPassword from "./pages/ForgotPassword";
+import UpdatePassword from "./pages/UpdatePassword";
+import VerifyEmail from "./pages/VerifyEmail";
+import About from "./pages/About";
+
 function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
@@ -29,7 +34,43 @@ function App() {
             </OpenRoute>
           }
         />
+        <Route
+          path="forgot-password"
+          element={
+            <OpenRoute>
+              <ForgotPassword />
+            </OpenRoute>
+          }
+        />
+
+        <Route
+          path="verify-email"
+          element={
+            <OpenRoute>
+              <VerifyEmail />
+            </OpenRoute>
+          }
+        />
+
+        <Route
+          path="update-password/:id"
+          element={
+            <OpenRoute>
+              <UpdatePassword />
+            </OpenRoute>
+          }
+        />  
+        <Route
+          path="about"
+          element={
+            <OpenRoute>
+              <About />
+            </OpenRoute>
+          }
+        />  
+
       </Routes>
+
     </div>
   );
 }
